601797fd6fb204326f4bc2790dbfe1e8ece96b1b
qjlhlh <qjlhlh@gamil.com>
2012-11-02 11:56:47 +0800

    tabbarview Controller用到的三个view controller

---
+++ BaseSample/OtherViewController.h
@@ -0,0 +1,13 @@
+//
+//  OtherViewController.h
+//  BaseSample
+//
+//  Created by qian jin on 12-11-2.
+//  Copyright (c) 2012年 newyulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherViewController : UIViewController
+
+@end
